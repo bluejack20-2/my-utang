e723b0e087d73a81ece6895e07b97164af22aa17
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-06-18 00:16:26 +0700

    update qr code button

--- resources/views/livewire/dashboard/show-qr-code.blade.php
+++ resources/views/livewire/dashboard/show-qr-code.blade.php
@@ -3,16 +3,15 @@
         <x-jet-secondary-button class="w-full justify-center" wire:click="$set('showModal', true)">
             qr code
         </x-jet-secondary-button>
+
+        <x-jet-modal wire:model="showModal">
+            <img
+                src="{{ asset(Storage::url($debt->creditor->payment_qr_code_path)) }}"
+                alt="payment qr code">
+        </x-jet-modal>
     @else
         <x-jet-secondary-button class="w-full justify-center" disabled>
             qr code
         </x-jet-secondary-button>
     @endif
-
-
-    <x-jet-modal wire:model="showModal">
-        <img
-            src="{{ asset(Storage::url($debt->creditor->payment_qr_code_path)) }}"
-            alt="payment qr code">
-    </x-jet-modal>
 </div>
