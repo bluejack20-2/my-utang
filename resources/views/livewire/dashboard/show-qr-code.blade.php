352dcca09227468502c3f8ff7b0dfbdf67306507
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-06-17 23:31:32 +0700

    Update show-qr-code.blade.php

--- resources/views/livewire/dashboard/show-qr-code.blade.php
+++ resources/views/livewire/dashboard/show-qr-code.blade.php
@@ -1,8 +1,14 @@
 <div>
-    <x-jet-secondary-button @if ($debt->creditor->payment_qr_code_path) disabled @endif class="w-full justify-center"
-                            wire:click="$set('showModal', true)">
-        qr code
-    </x-jet-secondary-button>
+    @if ($debt->creditor->payment_qr_code_path)
+        <x-jet-secondary-button class="w-full justify-center" wire:click="$set('showModal', true)">
+            qr code
+        </x-jet-secondary-button>
+    @else
+        <x-jet-secondary-button class="w-full justify-center" disabled>
+            qr code
+        </x-jet-secondary-button>
+    @endif
+
 
     <x-jet-modal wire:model="showModal">
         <img
